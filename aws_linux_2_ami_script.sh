b4141e526ff4070d809553b4136ecb78ac6b3867
Ragu Jayaraman <rjayaraman@vandis.com>
2020-01-27 00:55:34 -0500

    startup script v1

--- aws_linux_2_ami_script.sh
+++ aws_linux_2_ami_script.sh
@@ -1,10 +1,28 @@
 # Update and install pip3
-sudo apt-get update
-sudo apt-get install python3-pip
+sudo yum update -y
+sudo yum install python3-pip -y
+sudo yum install python-pip -y
+sudo yum install git -y
+sudo pip install --upgrade pip virtualenv
 
+# Account to own server process
+sudo useradd -m -d /home/pythonapp pythonapp
 
+# Fetch source code
+export HOME=/root
+sudo git clone https://github.com/RaguRJ/aws_instance_detail_flask_app.git /opt/app
 
-#Clone git repo and install requirements
-git clone https://github.com/RaguRJ/aws_instance_detail_flask_app.git
-pip3 install --user -r requirements.txt
+# Python environment setup
+sudo virtualenv -p python3 /opt/app/env
+source /opt/app/env/bin/activate
+sudo /opt/app/env/bin/pip install -r /opt/app/requirements.txt
 
+# Set ownership to newly created account
+sudo chown -R pythonapp:pythonapp /opt/app
+
+# Put supervisor configuration in proper place
+sudo cp /opt/app/python-app.conf /etc/supervisor/conf.d/python-app.conf
+
+# Start service via supervisorctl
+supervisorctl reread
+supervisorctl update
