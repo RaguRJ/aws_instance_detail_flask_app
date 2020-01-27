c7a41ed9d0ff41780fd773bccb2b3918db9387ac
Ragu Jayaraman <rjayaraman@vandis.com>
2020-01-27 14:17:58 -0500

    adding scritp files from home laptop

--- aws_linux_2_ami_script.sh
+++ aws_linux_2_ami_script.sh
@@ -4,6 +4,8 @@ sudo yum install python3-pip -y
 sudo yum install python-pip -y
 sudo yum install git -y
 sudo pip install --upgrade pip virtualenv
+sudo amazon-linux-extras install epel -y
+sudo yum install supervisor -y
 
 # Account to own server process
 sudo useradd -m -d /home/pythonapp pythonapp
@@ -11,6 +13,9 @@ sudo useradd -m -d /home/pythonapp pythonapp
 # Fetch source code
 export HOME=/root
 sudo git clone https://github.com/RaguRJ/aws_instance_detail_flask_app.git /opt/app
+
+# Supervisor configuration
+sudo cp /opt/app/supervisord.conf /etc/supervisord.conf
 
 # Python environment setup
 sudo virtualenv -p python3 /opt/app/env
@@ -21,7 +26,7 @@ sudo /opt/app/env/bin/pip install -r /opt/app/requirements.txt
 sudo chown -R pythonapp:pythonapp /opt/app
 
 # Put supervisor configuration in proper place
-sudo cp /opt/app/python-app.conf /etc/supervisor/conf.d/python-app.conf
+sudo cp /opt/app/python-app.conf /etc/supervisor.d/python-app.conf
 
 # Start service via supervisorctl
 supervisorctl reread
